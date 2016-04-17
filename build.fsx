a9090bdbf3915958b86f4ccddfa805e5179be743
Bart Wijnants <bartwijnants@outlook.com>
2016-04-17 21:06:30 +0200

    optimize references

--- build.fsx
+++ build.fsx
@@ -8,6 +8,11 @@ let outputFile = "app.exe"
 let outputPath = buildDir + outputFile
 let references = [ "./packages/FSharp.Core/lib/net40/FSharp.Core.dll" 
                    "./packages/Suave/lib/net40/Suave.dll" ]
+let fscParameters = [ FscHelper.FscParam.Out outputPath
+                      FscHelper.FscParam.Target FscHelper.TargetType.Exe
+                      FscHelper.FscParam.NoFramework ]          
+let fscParametersWithReferences = fscParameters @ 
+                                  List.map (fun reference -> FscHelper.FscParam.Reference reference) references                           
 
 Target "clean" (fun _ -> 
     CleanDirs [buildDir]
@@ -17,11 +22,7 @@ Target "default" (fun _ ->
     CreateDir buildDir
     CopyFiles buildDir references
     sourceFiles
-    |> FscHelper.Compile [ FscHelper.FscParam.Out outputPath
-                           FscHelper.FscParam.Target FscHelper.TargetType.Exe
-                           FscHelper.FscParam.Reference "./packages/FSharp.Core/lib/net40/FSharp.Core.dll"
-                           FscHelper.FscParam.Reference "./packages/Suave/lib/net40/Suave.dll"
-                           FscHelper.FscParam.NoFramework ]
+    |> FscHelper.Compile fscParametersWithReferences
 )
 
 "clean"
