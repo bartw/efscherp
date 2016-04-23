110ddf36eee7a5ff0d3098d65a74466c4275420a
Bart Wijnants <bartwijnants@outlook.com>
2016-04-23 07:46:57 +0200

    postgres

--- build.fsx
+++ build.fsx
@@ -6,8 +6,16 @@ let buildDir = "./bin/"
 let sourceFiles = ["./src/app.fs"]
 let outputFile = "app.exe"
 let outputPath = buildDir + outputFile
+let referencesToCopy = [ "./packages/FSharp.Core/lib/net40/FSharp.Core.dll" 
+                         "./packages/Suave/lib/net40/Suave.dll"
+                         "./packages/SQLProvider/lib/FSharp.Data.SqlProvider.dll"
+                         "./packages/Npgsql/lib/net45/Npgsql.dll" ]
 let references = [ "./packages/FSharp.Core/lib/net40/FSharp.Core.dll" 
-                   "./packages/Suave/lib/net40/Suave.dll" ]
+                   "./packages/Suave/lib/net40/Suave.dll"
+                   "./packages/SQLProvider/lib/FSharp.Data.SqlProvider.dll"
+                   "./packages/Npgsql/lib/net45/Npgsql.dll"
+                   "System"
+                   "System.Core" ]
 let fscParameters = [ FscHelper.FscParam.Out outputPath
                       FscHelper.FscParam.Target FscHelper.TargetType.Exe
                       FscHelper.FscParam.NoFramework ]          
@@ -20,7 +28,7 @@ Target "clean" (fun _ ->
 
 Target "default" (fun _ ->
     CreateDir buildDir
-    CopyFiles buildDir references
+    CopyFiles buildDir referencesToCopy
     sourceFiles
     |> FscHelper.Compile fscParametersWithReferences
 )
--- src/app.fs
+++ src/app.fs
@@ -2,7 +2,29 @@ open Suave
 open Suave.Filters
 open Suave.Operators
 open Suave.Successful
+open System
+open FSharp.Data.Sql
 
-let routes = choose [ path "/" >=> (OK "Home") ]
+let [<Literal>] connStr = @"Server=127.0.0.1;Database=cleverclass;User Id=suave;Password=1234;"
+let [<Literal>] dbVendor = Common.DatabaseProviderTypes.POSTGRESQL
+let [<Literal>] resolutionFolder = @"C:\Users\bartw\Documents\projects\efscherp\packages\Npgsql\lib\net45"
+let [<Literal>] useOptTypes  = true
+
+type Sql = 
+    SqlDataProvider< dbVendor, 
+                     connStr,  
+                     ResolutionPath = resolutionFolder, 
+                     UseOptionTypes = useOptTypes >
+
+type DbContext = Sql.dataContext
+type ClassGroup = DbContext.``public.classgroupsEntity``
+
+let getContext() = Sql.GetDataContext()
+
+let getClassGroups (ctx : DbContext) : ClassGroup list = 
+    ctx.Public.Classgroups |> Seq.toList
+
+let routes = choose [ path "/" >=> (OK "Home") 
+                      path "/classgroups" >=> (OK "classgroups")]
 
 startWebServer defaultConfig routes
